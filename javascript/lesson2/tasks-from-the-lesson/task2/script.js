f450485ba9adb1dd6f2d5b933d7e3315051234ab
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-22 10:21:30 +0200

    add

--- javascript/lesson2/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson2/tasks-from-the-lesson/task2/script.js
@@ -15,7 +15,7 @@ console.log('h:', typeof h);
 const i = undefined;
 console.log('i:', typeof i);
 const j = Symbol("I'm a Symbol");
-console.log();
+console.log('j:', typeof j);
 const k = { name: 'John' };
 console.log();
 const l = [1, 2, 3, 4];
