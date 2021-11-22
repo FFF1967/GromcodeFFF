b0cf55b6a190528acc247cbe82f5daa4ca193d27
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-22 20:44:00 +0200

    add

--- javascript/lesson2/tasks-from-the-lesson/task3/script.js
+++ javascript/lesson2/tasks-from-the-lesson/task3/script.js
@@ -1,4 +1,9 @@
 let goodNumber = 17;
+let anotherNumber = goodNumber;
 const anotherNumber = 77;
 console.log(goodNumber);
 console.log(anotherNumber);
+Object { customer: }
+
+const customer { name: 'John', age: 17 }
+const anotherCustomer = 20;
