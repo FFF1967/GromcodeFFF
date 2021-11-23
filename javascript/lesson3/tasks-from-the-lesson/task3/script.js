df738a065d31eb9f7fe73f11eeeb40a354d47eb0
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-23 20:55:25 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task3/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task3/script.js
@@ -1,10 +1,43 @@
-let goodNumber = 17;
-let anotherNumber = goodNumber;
-anotherNumber = 77;
-console.log(goodNumber);
-console.log(anotherNumber);
-let customer = { name: 'John', age: 17 };
-let anotherCustomer = customer;
-anotherCustomer.age = 20;
-console.log(customer.age);
-console.log(anotherCustomer.age);
+/* eslint-disable */
+
+/* Результатом сравнения является булевое значение */
+
+/* Сравнение чисел */
+const a = 17;
+const b = 5;
+a > b;
+a <= b;
+a == b;
+a != b;
+
+/* Сравнение строк */
+'a' < 'b';
+'ab' > 'a';
+
+/* При сравнении переменных разных типов они преобразуются в числа */
+'17' > 1;
+
+/* Строгое сравнение */
+17 === 1;
+17 === 17;
+17 === '17';
+17 === true;
+'0' === '';
+true === false;
+true === true;
+null === undefined;
+false === 0;
+
+/* Нестрогое сравнение */
+17 == '17';
+'0' == '';
+0 == '';
+null == undefined;
+false == 0;
+
+/* Осторожно c null и undefined */
+undefined == null;
+undefined == 0;
+null == 0;
+undefined < 0;
+undefined > 0;
