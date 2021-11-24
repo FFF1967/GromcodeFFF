889d51442e07199df22fd1fc0873d4db17bd2195
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 19:01:41 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task2/script.js
@@ -8,9 +8,9 @@ console.log('-17.17 to string is ' + String(-17.17));
 console.log('false to string is ');
 console.log('false to string' + String(false));
 console.log('null to string is ');
-console.log('' + String());
+console.log('null to string is ' + String(null));
 console.log('undefined to string is ');
-console.log('' + String());
+console.log('undefined' + String(undefined));
 console.log('0 to string is ');
 console.log('' + String());
 
