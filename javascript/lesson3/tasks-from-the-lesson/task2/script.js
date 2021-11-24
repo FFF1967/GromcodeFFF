2091f09b3386218290fdc658f5168c86c86d7f52
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 16:19:33 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task2/script.js
@@ -2,7 +2,7 @@
 
 /* cast to string */
 console.log('17 to string is ');
-console.log('17 to string is ' + String(-17));
+console.log('17 to string is ' + String(17));
 console.log('-17.17 to string is ');
 console.log('17.17 to string is ' + String(-17.17));
 console.log('false to string is ');
