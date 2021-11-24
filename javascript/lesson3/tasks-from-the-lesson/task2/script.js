244255cffe29f1f6faa9255eeebbd0b491e545ac
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 20:15:14 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task2/script.js
@@ -28,7 +28,7 @@ console.log('undefined to is ' + Number('undefined'));
 console.log("'   20   ' to number is ");
 console.log('"   20   " to number is ' + Number("   20   "));
 console.log("'      ' to number is ");
-console.log('' + String());
+console.log('"      " to number is ' + Number("     "));
 console.log("'   30d   ' to number is ");
 console.log('' + String());
 
