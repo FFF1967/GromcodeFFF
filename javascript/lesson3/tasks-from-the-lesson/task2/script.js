464a386085d4a289fcd90d6cd7cdb0330b04efdb
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 20:59:02 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task2/script.js
@@ -28,9 +28,9 @@ console.log('undefined to number is ' + Number(NaN));
 console.log("'   20   ' to number is ");
 console.log("'   20   ' to number is " + Number(20));
 console.log("'      ' to number is ");
-console.log('"      " to number is ' + Number(NaN));
+console.log("'      ' to number is " + Number(NaN));
 console.log("'   30d   ' to number is ");
-console.log('"   30d   " to number is ' + Number(NaN));
+console.log("'   30d   ' to number is " + Number(NaN));
 
 /* cast to boolean */
 console.log('null to boolean is ');
