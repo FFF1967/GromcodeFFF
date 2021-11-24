c69f9b53af1aad8d12b5bc973c9d7cb60d666336
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 13:36:51 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task2/script.js
+++ javascript/lesson3/tasks-from-the-lesson/task2/script.js
@@ -1,28 +1,30 @@
-const a = 17;
-console.log('a:', typeof a);
-const b = 17.17;
-console.log('b:', typeof b);
-const c = '17';
-console.log('c:', typeof c);
-const d = '17';
-console.log('d:', typeof d);
-const e = "I'm a string";
-console.log('e:', typeof e);
-const f = false;
-console.log('f:', typeof f);
-const g = true;
-console.log('g:', typeof g);
-const h = null;
-console.log('h:', typeof h);
-const i = undefined;
-console.log('i:', typeof i);
-const j = Symbol("I'm a Symbol");
-console.log('j:', typeof j);
-const k = { name: 'John' };
-console.log('k:', typeof k);
-const l = [1, 2, 3, 4];
-console.log('l:', typeof l);
-const m = function run() {
-    return 17;
-};
-console.log('m:', typeof m);
+// code below must be finished ;)
+
+/* cast to string */
+console.log('17 to string is ');
+console.log('-17.17 to string is ');
+console.log('false to string is ');
+console.log('null to string is ');
+console.log('undefined to string is ');
+console.log('0 to string is ');
+
+/* cast to number */
+console.log("'17' to number is ");
+console.log('true to number is ');
+console.log('false to number is ');
+console.log('null to number is ');
+console.log('undefined to number is ');
+console.log("'   20   ' to number is ");
+console.log("'      ' to number is ");
+console.log("'   30d   ' to number is ");
+
+/* cast to boolean */
+console.log('null to boolean is ');
+console.log('undefined to boolean is ');
+console.log('0 to boolean is ');
+console.log('-0 to boolean is ');
+console.log('NaN to boolean is ');
+console.log("'' to boolean is ");
+console.log("' ' to boolean is ");
+console.log('17 to boolean is ');
+console.log("'Hello' to boolean is ");
