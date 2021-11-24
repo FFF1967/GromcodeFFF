0cfceafae41627f0a8045905299e686868d0d504
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2021-11-24 08:32:34 +0200

    add

--- javascript/lesson3/tasks-from-the-lesson/task1/index.js
+++ javascript/lesson3/tasks-from-the-lesson/task1/index.js
@@ -39,13 +39,19 @@ console.log("-a: ", b);
 '24' / 4;
 console.log("'24' / 4: ", '24' / 4);
 18 * '3';
+console.log("18 * '3':", 18 * '3');
 '9' - '3';
+console.log("'9' - '3':", '9' - '3');
 'seventeen' / 3;
+console.log("seventeen' / 3:", 'seventeen' / 3);
 4 * 'four';
+console.log("4 * 'four':", 4 * 'four');
 17 / 0;
-
+console.log("17 / 0:", 17 / 0);
 10 + 'seven';
+console.log("10 + 'seven':", 10 + 'seven');
 undefined + 1;
+
 null + 8;
 undefined + '5';
 'text' + null;
