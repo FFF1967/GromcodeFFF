9abe8db46bcfe69e8eecbb6cd0bf287b7c56f65f
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-01-27 20:14:15 +0200

    add

--- javascript/lesson2/tasks-from-the-lesson/task5/index.js
+++ javascript/lesson2/tasks-from-the-lesson/task5/index.js
@@ -9,13 +9,21 @@ console.log('d:', typeof d);
 const e = "I'm a string";
 console.log('e:', typeof d);
 const f = false;
-
+console.log('f:', typeof f);
 const g = true;
+console.log('g:', typeof g);
 const h = null;
+console.log('h:', typeof h);
 const i = undefined;
+console.log('i:', typeof i);
 const j = Symbol("I'm a Symbol");
+console.log('j:', typeof j);
 const k = { name: 'John' };
+console.log('k:', typeof j);
 const l = [1, 2, 3, 4];
+console.log('l:', typeof l);
 const m = function run() {
     return 17;
+
 };
+console.log('m:', typeof m);
