3f0fc98c2fc7f496c997e475b7acbd83a70c66f1
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-01-27 21:09:35 +0200

    add

--- javascript/lesson2/tasks-from-the-lesson/task5/index.js
+++ javascript/lesson2/tasks-from-the-lesson/task5/index.js
@@ -1,10 +1,10 @@
-const goodNumber = 17;
+let goodNumber = 17;
 let anotherNumber = goodNumber;
 anotherNumber = 77;
-console.log('goodNumber');
-console.log('anotherNumber');
-const customer = { name: 'John', age: 17 };
+console.log(goodNumber);
+console.log(anotherNumber);
+let customer = { name: 'John', age: 17 };
 let anotherCustomer = customer;
-anotherNumber.age = 20;
-console.log('customer.age');
-console.log('anotherCustomer.age');
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
