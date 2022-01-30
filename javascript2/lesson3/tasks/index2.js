850b69a7ebbd77666e018b6ea7e729773b43f759
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-01-30 22:18:34 +0200

    add

---
+++ javascript2/lesson3/tasks/index2.js
@@ -0,0 +1,21 @@
+const a = 17;
+const b = 5;
+
+// Результатом сравнения является булево
+
+a > b; // true
+a < b; // false
+a <= b; // false
+a >= b; // true
+a == b; // false
+a != b; // true
+
+/* === */
+
+'a' < 'b'; // true
+'ab' > 'a'; // true
+'aa' <= 'a'; // false
+'aa' >= 'a'; // true
+'a' > 'A'; // true
+
+// При сравнении переменных разных типов
