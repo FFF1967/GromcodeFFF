ba9f1380c1d6b4b497c43142e4626dea326c1e16
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-02-01 17:49:06 +0200

    add

--- javascript2/lesson3/tasks/index2.js
+++ javascript2/lesson3/tasks/index2.js
@@ -19,3 +19,34 @@ a != b; // true
 'a' > 'A'; // true
 
 // При сравнении переменных разных типов
+
+'17' > 1; // true
+
+17 == '17'; // true
+
+17 === 1; // false
+17 === 17; // true
+17 === '17'; // false
+17 === true; // false
+'0' === ''; // false
+true === false; // false
+true === true; // true
+null === undefined; // flse
+false === 0; // false
+
+/* === */
+
+0 == ''; // true
+0 === ''; // false
+
+17 == '17'; //
+'0' == ''; //
+0 == '';
+null == undefined;
+false == 0;
+//
+undefined == null;
+undefined == 0;
+null == 0;
+undefined < 0;
+undefined > 0;
