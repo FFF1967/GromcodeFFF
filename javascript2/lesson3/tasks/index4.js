8840bf104f995afe1407d8ae2382754dc44ada19
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-02-03 18:03:01 +0200

    add

--- javascript2/lesson3/tasks/index4.js
+++ javascript2/lesson3/tasks/index4.js
@@ -6,3 +6,15 @@ if (true) { console.log('I\'m in'); } // undefined
 if ('text') { console.log('Yes'); } // Yes
 
 if (null) { console.log('Yes'); } else { console.log('Else'); } // Else
+
+
+const age = 17;
+if (age < 16) {
+    console.log('Too young');
+} else if (age >= 16 && age < 18) {
+    console.log('Almost ready');
+} else if (age >= 18 && age < 65) {
+    console.log('Ready');
+} else {
+    console.log('Else');
+}
