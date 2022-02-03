aec0265feb3512e2a6c69f6f4478e6034c3d815f
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-02-03 17:49:52 +0200

    add

--- javascript2/lesson3/tasks/index4.js
+++ javascript2/lesson3/tasks/index4.js
@@ -1,3 +1,8 @@
 if (true) console.log('I\'m in'); // I'm in
 
 if (true) { console.log('I\'m in'); } // undefined
+
+
+if ('text') { console.log('Yes'); }
+
+if (null) { console.log('Yes'); } else { console.log('Else'); }
