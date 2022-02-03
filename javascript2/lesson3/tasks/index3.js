270ee53c0703783552591050d5815666e22db152
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-02-03 17:39:16 +0200

    add

--- javascript2/lesson3/tasks/index3.js
+++ javascript2/lesson3/tasks/index3.js
@@ -11,4 +11,28 @@ true && true; // true
 
 // Логические операторы могут применяться к любым типам данных
 
-//   находит первое истинное значение
+//  || находит первое истинное значение
+
+'text' || false; // 'text'
+true || 'text'; // true
+undefined || null || 0 || '' || null; // null
+
+
+const customAmont = null;
+const defaultAmount = 17;
+const amount = customAmont || defaultAmount;
+
+
+// && находит первое ложное значение
+
+'text' && false; // false
+true && 'text'; // 'text'
+undefined && null && 0 && '' && null; // undefined; // undefined
+
+/* == */
+!true; // false
+!false; // true
+!'text'; // false
+!!'text'; // true
+
+// ! -> && ->
