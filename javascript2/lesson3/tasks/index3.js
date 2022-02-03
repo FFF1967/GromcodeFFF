57c7abb89c733f71cef2b9e5553dda21d0910af6
FFF1967 <86532360+FFF1967@users.noreply.github.com>
2022-02-03 17:21:30 +0200

    add

--- javascript2/lesson3/tasks/index3.js
+++ javascript2/lesson3/tasks/index3.js
@@ -1,7 +1,14 @@
 // оператор || (OR)
-false || true; //
-false || true || false
-false || false || false
-
+false || true; // true
+false || true || false; // true
+false || false || false; // false
 
 // оператор && (AND)
+false && true; // false
+false && true && false; // false
+true && true && true; // true
+true && true; // true
+
+// Логические операторы могут применяться к любым типам данных
+
+//   находит первое истинное значение
